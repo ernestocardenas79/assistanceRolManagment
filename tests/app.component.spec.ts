d79aa553cba51a97f55fb74042a930cee11ab224
Ernesto Cardenas LLanes <ernesto.cardenas79@gmail.com>
2022-09-02 21:22:58 -0600

    configurando jest

--- tests/app.component.spec.ts
+++ tests/app.component.spec.ts
@@ -3,18 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from '../src/app/app.component';
 
 describe('AppComponent', () => {
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
-
 
   test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -25,7 +19,7 @@ describe('AppComponent', () => {
   test(`should have as title 'angular-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const component = fixture.componentInstance;
-    expect(component.title).toEqual('Bienvenido a Administrador de asistencias a clases');
+    expect(component.title).toEqual('Administrador de asistencias a clases');
   });
 
   test('should render title', () => {
@@ -33,17 +27,15 @@ describe('AppComponent', () => {
     const component = fixture.componentInstance;
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    
+
     const h1 = compiled.querySelector('h1');
-    expect( h1?.textContent ).toContain( component.title );
+    expect(h1?.textContent).toContain(component.title);
   });
 
   test('debe hacer match con el snapshot', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect( compiled ).toMatchSnapshot();
-
-  })
-
+    expect(compiled).toMatchSnapshot();
+  });
 });
---
+++ tests/core/services/assistanceRoleMangment.spec.ts
@@ -0,0 +1,7 @@
+/// <reference types="jest" />
+
+describe('Un test suite', () => {
+  test('test ', () => {
+    expect(1).toBeTruthy();
+  });
+});
